37227d7b06b0a398f81713cc92b144988aff41b8
Mark Wolfe <mark@wolfe.id.au>
2026-01-11 20:07:27 +1100

    feat: migrate athena queries to aws SDK v2

--- internal/partitions/partitions_test.go
+++ internal/partitions/partitions_test.go
@@ -5,6 +5,8 @@ import "testing"
 func Test_buildQuery(t *testing.T) {
 	type args struct {
 		table string
+		year  string
+		month string
 	}
 	tests := []struct {
 		name    string
@@ -14,13 +16,13 @@ func Test_buildQuery(t *testing.T) {
 	}{
 		{
 			name: "should build valid query",
-			args: args{table: "table_name"},
-			want: "ALTER TABLE table_name ADD IF NOT EXISTS\nPARTITION (year=?, month=?)",
+			args: args{table: "table_name", year: "2023", month: "1"},
+			want: "ALTER TABLE table_name ADD IF NOT EXISTS\n    PARTITION (year = '2023', month = '1')",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := buildQuery(tt.args.table)
+			got, err := buildQuery(tt.args.table, tt.args.year, tt.args.month)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("buildQuery() error = %v, wantErr %v", err, tt.wantErr)
 				return
